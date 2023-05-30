86ecacc4b00a0b00072c33425d7f6a251f58af20
HariharanM <hariakash1117@gmail.com>
2023-05-30 16:30:54 +0530

    Include Collection of C#

--- HandsOn3/Program.cs
+++ HandsOn3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HandsOn3;
+using System.Collections;
 
 
 //Function
@@ -13,8 +14,53 @@ static void returnMethod()
        Console.WriteLine("Elements in list:"+i);
     }
     Console.WriteLine("List Count:" + list.Count);
-  
+    list.Reverse();
+    foreach (int i in list)
+    {
+        Console.WriteLine("Elements in Reverse list:" + i);
+    }
+    list.Clear();
+    Console.WriteLine(list.Count);
 }
 
 //Function doesn't need object
 returnMethod();
+//Collections HashTable
+Console.WriteLine("\nCollections HashTable");
+Hashtable hashtbl = new Hashtable();
+
+hashtbl.Add(0, "Hello");
+
+ICollection kkey = hashtbl.Keys;
+Console.WriteLine(hashtbl[0]);
+//Collection Stach
+Console.WriteLine("\nCollection Stach");
+Stack stack = new Stack();  
+stack.Push('i');
+stack.Push('r');
+stack.Push('a');
+stack.Push('h');
+Console.WriteLine("Before Pop");
+foreach (char i in stack)
+{
+    Console.Write(i+" ");
+}
+stack.Pop();
+Console.WriteLine("\nAfter pop");
+foreach (char i in stack)
+{
+    Console.Write(i+" ");
+}
+Console.WriteLine();
+//Collection queue
+Console.WriteLine("\nCollection queue");
+Queue q = new Queue();
+q.Enqueue("M");
+q.Enqueue("A");
+q.Enqueue("U");
+q.Enqueue("I");
+
+foreach(string j in q)
+{
+    Console.WriteLine(j);
+}
