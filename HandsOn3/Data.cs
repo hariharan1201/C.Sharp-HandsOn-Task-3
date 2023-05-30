74bfa45166c815a85bc5603e3fb97ad6da4c29e9
HariharanM <hariakash1117@gmail.com>
2023-05-30 14:39:03 +0530

    Add project files.

---
+++ HandsOn3/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOn3
+{
+    internal class Data
+    {
+        //Method declare inside the class
+        public dynamic arrayFunc(int a)
+        {
+            List<int> list = new List<int>();
+            for(int i=1;i<=a;i++)
+            {
+                if(i%2 == 0)
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ HandsOn3/Program.cs
@@ -0,0 +1,20 @@
+﻿using HandsOn3;
+
+
+//Function
+static void returnMethod()
+{
+    Data data = new Data();
+    List<int> list = new List<int>();
+    //method associate with object data
+    list = data.arrayFunc(10);
+    foreach (int i in list)
+    {
+       Console.WriteLine("Elements in list:"+i);
+    }
+    Console.WriteLine("List Count:" + list.Count);
+  
+}
+
+//Function doesn't need object
+returnMethod();
